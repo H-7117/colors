37aa2db181a03b4f3f6205a554feb373999b60b9
H-7117 <hamod2131.a@gmail.com>
2024-05-22 18:15:13 +0300

    complete little fun game

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+function genearetColor() {
+  const digits = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
 
+  const color = new Array(6)
+    .fill("")
+    .map(() => digits[Math.floor(Math.random() * digits.length)])
+    .join("");
+  return `#${color}`;
+}
+
+enum Result {
+  Correct,
+  Wrong,
+}
 function App() {
+  const [color, setColor] = useState("");
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [result, setResult] = useState<Result | undefined | null>(undefined);
+
+  
+  function pickColor(){
+    const actualColor = genearetColor();
+      setColor(actualColor);
+      setAnswers([actualColor,genearetColor(),genearetColor()].sort(() => 0.5 - Math.random()));
+  }
+
+  useEffect(() => {
+    pickColor();
+  }, []);
+
+  function handlerAnswerClicked(answer: string) {
+    if (answer === color) {
+      setResult(Result.Correct);
+      pickColor();
+    } else {
+      setResult(Result.Wrong);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        
+        <div className="guess-me" style={{ background: color }}></div>
+        {answers.map((answer) => (
+          <button onClick={() => handlerAnswerClicked(answer)} key={answer}>
+            {answer}
+          </button>
+        ))}
+        {result === Result.Wrong && <div className="wrong">Wrong</div>}
+        {result === Result.Correct && <div className="correct">Correct</div>}
+      </div>
     </div>
   );
 }
